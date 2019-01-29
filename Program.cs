23b9e413269f0d0c9c085073f62f5da3d891fecd
ntfox <nt_fox@foxmail.com>
2019-01-29 12:21:52 +0800

    去掉csv，回头单独生成工程，不混在一起了

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace excel2json
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Excel.Application app = new Excel.Application();
 
@@ -102,5 +102,107 @@ namespace excel2json
             }
             app.Quit();
         }
+
+        static void Main(string[] args)
+        {
+            Excel.Application app = new Excel.Application();
+            bool needPressKey = true;
+            string srcPath = "", destPath = ".";
+            bool allSheet = true, needDataType = true;
+
+            List<string> argsList = new List<string>(args);
+
+            allSheet = GetTag(argsList, "allSheet");
+            needDataType = GetTag(argsList, "needDataType");
+
+            switch (argsList.Count)
+            {
+                case 1:
+                    {
+                        srcPath = args[0];
+                        break;
+                    }
+                case 2:
+                    {
+                        srcPath = GetFileAndDirectory(args[0]);
+                        destPath = GetFileAndDirectory(args[1]);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("1, excel2json excelFile");
+                        Console.WriteLine("2, excel2json outDir allSheet needDataType");
+                        Console.WriteLine("3, excel2json srcDir outDir allSheet needDataType");
+                        Console.WriteLine("4, excel2json jsonFile");
+
+                        return;
+                    }
+            }
+            srcPath = System.IO.Path.GetFullPath(srcPath);
+            destPath = System.IO.Path.GetFullPath(destPath);
+            if (System.IO.File.Exists(srcPath))
+            {
+                if (System.IO.File.Exists(destPath))
+                {
+                    needPressKey = !ExcelToJson.Process(app, srcPath, destPath, allSheet, needDataType);
+                }
+                else
+                {
+                    destPath = System.IO.Path.Combine(destPath, System.IO.Path.GetFileName(srcPath));
+                    needPressKey = !ExcelToJson.Process(app, srcPath, destPath, allSheet, needDataType);
+                }
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(srcPath) && System.IO.Directory.Exists(destPath))
+                {
+                    string[] srcfiles = System.IO.Directory.GetFiles(srcPath, "*.xlsx");
+                    for (int i = 0; i < srcfiles.Length; i++)
+                    {
+                        string srcFilename = srcfiles[i];
+                        string prename = System.IO.Path.GetFileNameWithoutExtension(srcFilename).Substring(0, 2);
+                        if (prename == "~$")
+                        {
+                            continue;
+                        }
+                        Console.WriteLine("                     --------  {0} --------", System.IO.Path.GetFileNameWithoutExtension(srcFilename));
+                        string targetFilename = destPath + System.IO.Path.GetFileNameWithoutExtension(srcFilename) + ".json";
+                        bool rt = ExcelToJson.Process(app, srcFilename, targetFilename, allSheet, needDataType);
+                        needPressKey = rt && needPressKey;
+                    }
+                    needPressKey = !needPressKey;
+                }
+            }
+
+            if (needPressKey)
+            {
+                Console.Write("press any key to quit:");
+                Console.ReadKey(true);
+            }
+            app.Quit();
+        }
+
+        static string GetFileAndDirectory(string path)
+        {
+            if (System.IO.Directory.Exists(path) || System.IO.File.Exists(path))
+            {
+                return path;
+            }
+            return "";
+        }
+
+        static bool GetTag(List<string> argsList, string tag)
+        {
+            for (int i = 0; i < argsList.Count; i++)
+            {
+                if (argsList[i].ToLower() == tag.ToLower())
+                {
+                    argsList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
+
 }
